7d399dc04f593adaafbaf8ba62234a0e4e82a7b0
swapnilhk <swapnilhk@gmail.com>
2021-06-11 19:51:13 +0530

    Update Session.java

--- src/main/java/com/swapnilhk/cowinappointmenttracker/model/Session.java
+++ src/main/java/com/swapnilhk/cowinappointmenttracker/model/Session.java
@@ -6,15 +6,17 @@ import java.util.Date;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class Session {
 	@JsonProperty("session_id")
 	String sessionId;
         @JsonProperty("date")
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
-	Date date;
+        Date date;
         @JsonProperty("available_capacity")
         Double availableCapacity;
         @JsonProperty("min_age_limit")
@@ -27,4 +29,29 @@ public class Session {
         Double availableCapacityDose1;
         @JsonProperty("available_capacity_dose2")
         Double availableCapacityDose2;
+        
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((sessionId == null) ? 0 : sessionId.hashCode());
+			return result;
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Session other = (Session) obj;
+			if (sessionId == null) {
+				if (other.sessionId != null)
+					return false;
+			} else if (!sessionId.equals(other.sessionId))
+				return false;
+			return true;
+		}
+        
 }
